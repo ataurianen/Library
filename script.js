c9bca0ddd8b21ae02eeab9b2b809a5923e6ba114
Andrew Taurianen <andrew.taurianen@gmail.com>
2023-02-06 21:23:06 -0800

    Added table that displays books

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const bookshelf = document.getElementById("bookshelf-body");
 
 function Book(title, author, numPages, haveRead) {
   this.title = title;
@@ -14,19 +15,21 @@ function Book(title, author, numPages, haveRead) {
 
 function addBookToLibrary(title, author, numPages, haveRead) {
   newBook = new Book(title, author, numPages, haveRead);
-
   myLibrary.push(newBook);
-}
+  let row = bookshelf.insertRow(-1);
 
-function printLibrary() {
-  myLibrary.forEach((novel) => {
-    console.log(novel.info());
-  });
+  let c1 = row.insertCell(0);
+  let c2 = row.insertCell(1);
+  let c3 = row.insertCell(2);
+  let c4 = row.insertCell(3);
+
+  c1.textContent = title;
+  c2.textContent = author;
+  c3.textContent = numPages;
+  c4.textContent = haveRead;
 }
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkin", 245, true);
 addBookToLibrary("The Fellowship of the Ring", "J.R.R. Tolkin", 654, true);
 addBookToLibrary("The Two Towers", "J.R.R. Tolkin", 654, false);
 addBookToLibrary("The Return of the King", "J.R.R. Tolkin", 777, false);
-
-printLibrary();
